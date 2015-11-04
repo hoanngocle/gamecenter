652a3b0bb641143f7614e0c8e9bab87fe2a7e3fb
Beo Sagittarius <hoancn1.ptit@gmail.com>
2015-11-04 17:10:07 +0700

    register // DONE

--- js/login_modal.js
+++ js/login_modal.js
@@ -16,7 +16,7 @@ $(function () {
     var $divForms = $('#div-forms');
     var $modalAnimateTime = 300;
     var $msgAnimateTime = 150;
-    var $msgShowTime = 4000;
+    var $msgShowTime = 20000;
 
     $("form").submit(function () {
         switch (this.id) {
@@ -92,13 +92,13 @@ $(function () {
                 var $rg_username = $('#register_username').val();
                 var $rg_email = $('#register_email').val();
                 var $rg_password = $('#register_password').val();
-              var $rg_gender = $('select[name=gender]').val();
-//                var $rg_gender = 'Male';
+                var $rg_gender = $('select[name=gender]').val();
+
                 var $rg_day = $('select[name=day]').val();
                 var $rg_month = $('select[name=month]').val();
                 var $rg_year = $('select[name=year]').val();
                 var $rg_dateofbirth = "" +$rg_day + '-' + $rg_month + '-' + $rg_year + "";
-//var $rg_dateofbirth = '05/12/1993';
+
                 var username = /^([a-zA-Z0-9.])+$/;
                 var email = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i;
 
@@ -115,13 +115,24 @@ $(function () {
                     msgChange($('#div-register-msg'), $('#icon-register-msg'), $('#text-register-msg'), "error", "glyphicon-remove", "Lastname is required");
                 } else if ($rg_username == ""){
                     msgChange($('#div-register-msg'), $('#icon-register-msg'), $('#text-register-msg'), "error", "glyphicon-remove", "Username is required");
+                } else if ($rg_username.length < 6 || $rg_username.length > 22 ){
+                    msgChange($('#div-register-msg'), $('#icon-register-msg'), $('#text-register-msg'), "error", "glyphicon-remove", "Username must be between 6 and 22 character");                    
+                } else if (!username.test($rg_username)){
+                    msgChange($('#div-register-msg'), $('#icon-register-msg'), $('#text-register-msg'), "error", "glyphicon-remove", "Username is invalid");
                 } else if ($rg_email == ""){
                     msgChange($('#div-register-msg'), $('#icon-register-msg'), $('#text-register-msg'), "error", "glyphicon-remove", "Email address is required");
+                } else if (!email.test($rg_email)){
+                    msgChange($('#div-register-msg'), $('#icon-register-msg'), $('#text-register-msg'), "error", "glyphicon-remove", "Email address is invalid");
                 } else if ($rg_password == ""){
                     msgChange($('#div-register-msg'), $('#icon-register-msg'), $('#text-register-msg'), "error", "glyphicon-remove", "Password is required");
-                }
-                
-                
+                }  else if ($rg_day == "" || $rg_month == "" || $rg_year == ""){
+                    msgChange($('#div-register-msg'), $('#icon-register-msg'), $('#text-register-msg'), "error", "glyphicon-remove", "Date Of Birth is required");
+                } else if ($rg_day == "31" && ( $rg_month == "4" || $rg_month == "6" || $rg_month == "9" || $rg_month == "11" )){
+                    msgChange($('#div-register-msg'), $('#icon-register-msg'), $('#text-register-msg'), "error", "glyphicon-remove", "Date Of Birth is invalid");                    
+                } else if ($rg_month == "2" && ( $rg_day == "30" || $rg_day == "31" || $rg_day == "29")){
+                    msgChange($('#div-register-msg'), $('#icon-register-msg'), $('#text-register-msg'), "error", "glyphicon-remove", "Date Of Birth is invalid");                   
+                } 
+                          
                 //==========================
                 else {
                     $.ajax({
