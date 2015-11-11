76bf953336fa66630899bfeb42a3f248cd3120de
Beo Sagittarius <hoancn1.ptit@gmail.com>
2015-11-11 15:39:33 +0700

    DONE: ADD - VIDEO - IMAGE

--- admin/add_image.php
+++ admin/add_image.php
@@ -1,54 +1,57 @@
 <?php 
 	include('../includes/backend/mysqli_connect.php'); 
 	include('../includes/functions.php');
-?>
 
-<?php 
     $title_page = 'Add Gallery';
 	if ($_SERVER['REQUEST_METHOD'] == 'POST'){ // gia tri ton tai, xu ly form
 		// create variable error
 		$errors = array();
         $uid = $_SESSION['uid'];
-		// kiem tra page name co gia tri hay khong
+        
+		// validate title
 		if (empty($_POST['title'])) {
 			$errors[] = "title";
 		} else {
 			$title = mysqli_real_escape_string($dbc, strip_tags($_POST['title']));
 		}
 
-		// kiem tra xem type co gia tri hay ko
+		// validate type
 		if (isset($_POST['type']) && filter_var($_POST['type'], FILTER_VALIDATE_INT, array('min_range' => 1))) {
 			$type_id = $_POST['type'];
 		}else{
 			$errors[] = 'type';
 		}
 
-		// kiem tra image co gia tri hay khong
+		// validate Image
 		if (empty($_FILES['myImage']['name'])) {
 			$errors[] = "myImage";
 		} else {
-			$myImage =  $_FILES['myImage']['name'];
+            $check = checkImage($_FILES['myImage']['name']);
+            if(mysqli_num_rows($check) > 0){
+                $errors[] = 'existIMG';
+            }else {
+              $myImage =  $_FILES['myImage']['name'];  
+            }
 		}
         
-        //kiem tra trang thai bai viet co gia tri hay ko
+        // validate status
         if (isset($_POST['status'])) {
             $status = $_POST['status'];
         }else{
             $errors[] = 'status';
         }
 
-		// kiem tra xem co loi hay khong
+        
 		if (empty($errors)) {
-			// neu ko co loi xay ra bat dau chen vao CSDL
-			$query = "INSERT INTO tblimages (user_id, title, type_id, image, status, create_date )
-						VALUES ({$uid}, '{$title}', {$type_id}, '{$myImage}', '{$status}', NOW())";			
-			$result = mysqli_query($dbc, $query);
-			// ham tra ve ket qua co dung hay ko
-			confirm_query($result, $query);
-
+            // upload img      
+            $targetimg = '../images/gallery/'.$myImage;                  
+            move_uploaded_file($_FILES['myImage']['tmp_name'], $targetimg  );
+            
+			$result = addImage($uid, $type_id, $title, $myImage, $status);
+            
 			if (mysqli_affected_rows($dbc) == 1) {
                 echo "<script type='text/javascript'>
-                        alert('{$lang['ADD_OK']}');
+                        alert('{$lang['AD_IMG_SUCCESS']}');
                         window.location = 'list_images.php';
                         </script>      
                     ";
@@ -65,46 +68,46 @@
     } // END main IF submit condition
 	include('../includes/backend/header-admin.php');    
 ?>
-<!-- Script ################## -->
 	<div class="content-wrapper">
         <div class="container">
     		<div class="row">
                 <div class="col-md-12">
-                    <h1 class="page-head-line"><?= $lang['Manage Images']?></h1>
+                    <h1 class="page-head-line"><?= $lang['ADD_IMAGE_PAGE_HEADER'] ?></h1>
                 </div>
         	</div>
 
         	<div class="row">
-                <div class="col-md-11" style="margin-left: 47.25px">
+                <div class="col-md-11" style="margin-left: 4.1%">
                     <div class="panel panel-default">
                         <div class="panel-heading" style="text-align: center">
-                            <h2><?= $lang['Upload Images']?></h2>
-                            <h4><a href="index.php"><?= $lang['Home']?></a> / <a href="list_images.php"><?= $lang['List Images']?></a></h4>
+                            <h2><?= $lang['ADD_IMAGE_H2'] ?></h2>
+                            <h4><a href="index.php"><?= $lang['ADD_IMAGE_LINK_HOME'] ?></a> / <a href="list_images.php"><?= $lang['ADD_IMAGE_LINK_LIST']?></a></h4>
                         </div> <!-- END PANEL HEADING--> 
 						<?php if(!empty($error)) : ?>
-                            <div class='alert alert-danger' style='font-size: 18px; margin: 25px 35px'>
+                            <div class='message-error alert alert-danger'>
                                 <p><?= $error?></p>
                             </div>
             			<?php endif; ?>
     <!-- ================================== Form Add Images [start] ===================================== -->
                    		<div class="panel-body" style="margin: 0 20px 0 20px">
 							<form id="add_news" action="" method="post" enctype="multipart/form-data">
+                                
                                 <!-- ================= Title [start] =================== -->
-								<div class="form-group"  style="font-size: 18px" >
-								   	<label for="title"><?= $lang['Title'] ?></label>
-                                    <input style="font-size: 18px; height: 44px" type="text" class="form-control" id="title" name="title" placeholder="<?= $lang['Enter_title']?>  " value="<?php if(isset($title)) echo $title ?>" />
+								<div class="label-fontsize form-group" >
+								   	<label for="title"><?= $lang['ADD_IMAGE_FORM_TITLE_LABEL']?></label>
+                                    <input style="height: 44px" type="text" class="label-fontsize form-control" id="title" name="title" placeholder="<?= $lang['ADD_IMAGE_FORM_TITLE_TEXT']?>  " value="<?php if(isset($title)) : echo $title; endif; ?>" />
 								<?php if(isset($errors) && in_array('title', $errors)) : ?>
-                                    <div class='alert alert-warning' style='font-size: 16px; padding: 5px 5px 5px 12px; margin-top: 15px'>
-                                        <p><?= $lang['AD_Title_required'] ?></p>
+                                    <div class='message alert alert-warning'>
+                                        <p><?= $lang['ADD_IMAGE_FORM_TITLE_REQUIRED'] ?></p>
                                     </div>
                                 <?php endif; ?>
 								</div>
                                 
 								<!-- ================= Type [start] ===================== -->
-				     			<div class="form-group" style="font-size: 18px">
-				                    <label><?= $lang['Select_Type']?></label>
+				     			<div class="label-fontsize form-group" >
+				                    <label><?= $lang['ADD_IMAGE_FORM_TYPE'] ?></label>
 				                    
-				                    <select name="type" class="form-control" style="font-size: 18px; height: 44px">
+				                    <select name="type" class="label-fontsize form-control" style="height: 44px">
 				                        <option>-------</option>
 				                        <?php 
 											$query = "SELECT type_id, type_name FROM tbltypes WHERE cat_id = 3 ORDER BY type_id ASC";
@@ -119,45 +122,50 @@
 										 ?>
 				                    </select>
 				                    <?php if (isset($errors) && in_array('type', $errors)) : ?>
-										<div class='alert alert-warning' style='font-size: 14px; padding: 5px 5px 5px 12px; margin-top: 15px'>
-											<p><?= $lang['AD_Type_required'] ?></p>
+										<div class='message alert alert-warning'>
+											<p><?= $lang['ADD_IMAGE_FORM_TYPE_REQUIRED']?></p>
 	                    				</div>
                                     <?php endif; ?>
 				                </div>
 
 								<!-- ================= Image [start] ===================== -->
-								<div class="form-group" style="font-size: 18px">
-								    <label for="image"><?= $lang['Images Input'] ?></label> <br>
-								    <img id="image" style="width: 300px; height: 300px;" />
-									<input  name="myImage"  style="margin-top: 15px" id="uploadImage" type="file" onchange="PreviewImage();" />
+								<div class="label-fontsize form-group" >
+								    <label for="image"><?= $lang['ADD_IMAGE_FORM_IMG'] ?></label><br>
+								    <img id="image" class="image" />
+									<input name="myImage"  style="margin-top: 15px" id="uploadImage" type="file" onchange="PreviewImage();" />
                                     <?php if (isset($errors) && in_array('myImage', $errors)) : ?>
-										<div class='alert alert-warning' style='font-size: 14px; padding: 5px 5px 5px 12px; margin-top: 15px'>
-                                            <p><?= $lang['AD_image_required'] ?></p>
+										<div class='message alert alert-warning'>
+                                            <p><?= $lang['ADD_IMAGE_FORM_IMG_REQUIRED'] ?></p>
+                                        </div>
+                                    <?php endif; ?>
+                                    <?php if(isset($errors) && in_array('existIMG', $errors)) : ?>
+                                        <div class='message alert alert-warning'>
+                                            <p><?= $lang['ADD_IMAGE_FORM_IMG_EXIST'] ?></p>
                                         </div>
                                     <?php endif; ?>
 								</div>
-                                    
-								
+                                    	
                                 <!-- ================= Status: default is 0 [start] ===================== -->
-                                <div class="form-group" style="font-size: 18px">
-				                    <label><?= $lang['Select_Status']?></label>
+                                <div class="label-fontsize form-group" >
+				                    <label><?= $lang['ADD_IMAGE_FORM_STATUS']?></label>
 
-				                    <select name="status" class="form-control" style="font-size: 18px; height: 44px">
-				                        <option value="0"><?= $lang['Inactive']?></option>
-				                        <option value="1"><?= $lang['Active']?></option>
+				                    <select name="status" class="label-fontsize form-control" style="height: 44px">
+				                        <option value="0"><?= $lang['ADD_IMAGE_FORM_STATUS_VAL_0']?></option>
+				                        <option value="1"><?= $lang['ADD_IMAGE_FORM_STATUS_VAL_1']?></option>
 				                    </select>
 				                </div>
                                 
-								<!-- ================= Submit & Reset Button [start] ===================== -->
+								<!-- ================= Submit, Reset & Back Button [start] ===================== -->
 								<center >
-									<input type="submit" name="submit" class="btn btn-success" style="font-size: 18px; height: 44px; margin-right: 10px"  value="<?= $lang['Submit']?>">
-									<button type="reset" class="btn btn-danger" style="font-size: 18px; height: 44px"><?= $lang['Reset']?></button>
+									<input type="submit" name="submit" class="btncustom btn btn-success" style="margin-right: 5px"  value="<?= $lang['BUTTON_ADD'] ?>">
+                                    <input type="reset" class="btncustom btn btn-warning" style="margin-right: 5px" value="<?= $lang['BUTTON_RESET'] ?>">
+                                    <input type="button" class="btncustom btn btn-danger" onclick="window.history.back();" value="<?= $lang['BUTTON_BACK'] ?>">
 								</center>							
-							</form> <!-- END FORM ADD IMAGE-->				 
+							</form> <!-- END FORM ADD VIDEO-->				 
 						</div> 
 		          	</div> <!-- END PANEL BODY-->
 				</div>
-    <!-- ================================== Form Add IMAGE [end] ===================================== -->		
+    <!-- ================================== FORM ADD VIDEO [end] ===================================== -->		
 			</div>
 		</div> >
     </div>
--- admin/add_video.php
+++ admin/add_video.php
@@ -1,113 +1,101 @@
 <?php 
 	include('../includes/backend/mysqli_connect.php'); 
 	include('../includes/functions.php');
-?>
-
-<?php    
+   
     $vid = $_GET['vid'];         
     if(empty($vid) || strlen($vid) != 11){
         redirect_to('admin/list_videos.php');       
     }else {
-  
-            $title_page = 'Add Video';
-            $video = get_youtube($vid);
-            $title = $video['title'];
-            if ($_SERVER['REQUEST_METHOD'] == 'POST'){ 
-                // create variable error
-                $errors = array();
-                $uid = $_SESSION['uid'];
+        $title_page = 'Add Video';
+        $video = get_youtube($vid);
+        $title = $video['title'];
+        if ($_SERVER['REQUEST_METHOD'] == 'POST'){ 
+            // create variable error
+            $errors = array();
+            $uid = $_SESSION['uid'];
 
+            // validate type
+            if (isset($_POST['type']) && filter_var($_POST['type'], FILTER_VALIDATE_INT, array('min_range' => 1))) {
+                $type_id = $_POST['type'];
+            }else{
+                $errors[] = 'type';
+            }
 
-                // kiem tra xem type co gia tri hay ko
-                if (isset($_POST['type']) && filter_var($_POST['type'], FILTER_VALIDATE_INT, array('min_range' => 1))) {
-                    $type_id = $_POST['type'];
-                }else{
-                    $errors[] = 'type';
-                }
+            // validate description
+            if (empty($_POST['description'])) {
+                $errors[] = "description";
+            } else {
+                $description = mysqli_real_escape_string($dbc, strip_tags($_POST['description']));
+            }
 
-                if (empty($_POST['description'])) {
-                    $errors[] = "description";
+            $url_video = "https://www.youtube.com/watch?v=$vid";
+            $url_thumbnail ="http://img.youtube.com/vi/$vid/sddefault.jpg";
+
+            // validate status
+            if (isset($_POST['status'])) {
+                $status = $_POST['status'];
+            }else{
+                $errors[] = 'status';
+            }
+
+            $thumbnail = save_thumbnail_from_url($url_thumbnail, $vid);
+            if (empty($errors)) {
+                		
+                $result = addVideo($uid, $type_id, $title, $description, $thumbnail, $url_video, $status);
+
+                if (mysqli_affected_rows($dbc) == 1) {
+                    echo "<script type='text/javascript'>
+                            alert('{$lang['AD_VIDEO_SUCCESS']}');
+                            window.location = 'list_videos.php';
+                            </script>      
+                        ";
                 } else {
-                    $description = mysqli_real_escape_string($dbc, strip_tags($_POST['description']));
+                    echo "<script type='text/javascript'>
+                            alert('{$lang['AD_FAIL']}');
+                            window.location = 'list_videos.php';
+                            </script>      
+                        ";
                 }
-
-                // kiem tra xem url video co gia tri hay ko
-
-                $url_video = "https://www.youtube.com/watch?v=$vid";
-                $url_thumbnail ="http://img.youtube.com/vi/$vid/sddefault.jpg";
-
-                //kiem tra trang thai bai viet co gia tri hay ko
-                if (isset($_POST['status'])) {
-                    $status = $_POST['status'];
-                }else{
-                    $errors[] = 'status';
-                }
-
-                $thumbnail = save_thumbnail_from_url($url_thumbnail, $vid);
-                // kiem tra xem co loi hay khong
-                if (empty($errors)) {
-                    // neu ko co loi xay ra bat dau chen vao CSDL
-                    $query = "INSERT INTO tblvideos (user_id, title, type_id, description, thumbnail, url_video, status, create_date )
-                                VALUES ({$uid}, '{$title}', {$type_id}, '{$description}', '{$thumbnail}', '{$url_video}', '{$status}', NOW())";			
-                    $result = mysqli_query($dbc, $query);
-                    // ham tra ve ket qua co dung hay ko
-                    confirm_query($result, $query);
-
-                    if (mysqli_affected_rows($dbc) == 1) {
-                        echo "<script type='text/javascript'>
-                                alert('{$lang['ADD_OK']}');
-                                window.location = 'list_videos.php';
-                                </script>      
-                            ";
-                    } else {
-                        echo "<script type='text/javascript'>
-                                alert('{$lang['ADD_FAIL']}');
-                                window.location = 'list_videos.php';
-                                </script>      
-                            ";
-                    }
-                } else {
-                    $error = $lang['AD_REQUIRED'];
-                }
+            } else {
+                $error = $lang['AD_REQUIRED'];
             }
         }
-    
+    }  
     include('../includes/backend/header-admin.php');
-
 ?>
-<!-- Script ################## -->
 	<div class="content-wrapper">
         <div class="container">
     		<div class="row">
                 <div class="col-md-12">
-                    <h1 class="page-head-line"><?= $lang['Manage_Videos']?></h1>
+                    <h1 class="page-head-line"><?= $lang['ADD_VIDEO_PAGE_HEADER']?></h1>
                 </div>
         	</div>
 
         	<div class="row">
-                <div class="col-md-11" style="margin-left: 47.25px">
+                <div class="col-md-11" style="margin-left: 4.1%">
                     <div class="panel panel-default">
                         <div class="panel-heading" style="text-align: center">
-                            <h2><?= $lang['Add_Videos']?></h2>
-                            <h4><a href="index.php"><?= $lang['Home']?></a> / <a href="list_videos.php"><?= $lang['List_Videos']?></a></h4>
+                            <h2><?= $lang['ADD_VIDEO_H2']?></h2>
+                            <h4><a href="index.php"><?= $lang['ADD_VIDEO_LINK_HOME']?></a> / <a href="list_videos.php"><?= $lang['ADD_VIDEO_LINK_LIST']?></a></h4>
                         </div> <!-- END PANEL HEADING-->  
                         <?php if(!empty($error)) : ?>
-                            <div class='alert alert-danger' style='font-size: 18px; margin: 25px 35px'>
+                            <div class='message-error alert alert-danger'>
                                 <p><?= $error?></p>
                             </div>
             			<?php endif; ?>
     <!-- ================================== Form Add Images [start] ===================================== -->
                    		<div class="panel-body" style="margin: 0 20px 0 20px">
 							<form id="add_videos" action="" method="post" >
+                                
                                 <!-- ================= Title [start] =================== -->
-								<div class="form-group"  style="font-size: 18px" >
-								   	<label for="title"><?= $lang['Title'] ?></label>
-                                    <input style="font-size: 18px; height: 44px" type="text" class="form-control" id="title" name="title" placeholder="<?= $lang['Enter_title']?> " value="<?= $video['title'] ?>"/>
+								<div class="label-fontsize form-group">
+								   	<label for="title"><?= $lang['ADD_VIDEO_FORM_TITLE_LABEL'] ?></label>
+                                    <input style="height: 44px" type="text" class="label-fontsize form-control" id="title" name="title" placeholder="<?= $lang['ADD_VIDEO_FORM_TITLE_TEXT']?> " value="<?= $video['title'] ?>"/>
 								</div>
                                 
 								<!-- ================= Type [start] ===================== -->
-				     			<div class="form-group" style="font-size: 18px">
-				                    <label><?= $lang['Select_Type']?></label>
+				     			<div class="label-fontsize form-group" >
+				                    <label><?= $lang['ADD_VIDEO_FORM_TYPE']?></label>
 				                    
 				                    <select name="type" class="form-control" style="font-size: 18px; height: 44px">
 				                        <option>-------</option>
@@ -124,43 +112,44 @@
 										 ?>
 				                    </select>
 				                    <?php if (isset($errors) && in_array('type', $errors)) : ?>
-										<div class='alert alert-warning' style='font-size: 14px; padding: 5px 5px 5px 12px; margin-top: 15px'>
-											<p><?= $lang['AD_Type_required'] ?></p>
+										<div class='message alert alert-warning'>
+											<p><?= $lang['ADD_VIDEO_FORM_TYPE_REQUIRED'] ?></p>
 	                    				</div>
                                     <?php endif; ?>
 				                </div>
 
 								<!-- ================= Url_video [start] =================== -->
-								<div class="form-group"  style="font-size: 18px" >
-                                    <label for="title"><?= $lang['Url_Videos']?></label>
-                                    <input style="font-size: 18px; height: 44px" type="text" class="form-control" id="url" name="url" size="20" maxlength="150" value="https://www.youtube.com/watch?v=<?= $vid ?>" disabled/>
+								<div class="label-fontsize form-group" >
+                                    <label for="title"><?= $lang['ADD_VIDEO_FORM_URL']?></label>
+                                    <input style="height: 44px" type="text" class="label-fontsize form-control" id="url" name="url" size="20" maxlength="150" value="https://www.youtube.com/watch?v=<?= $vid ?>" disabled/>
 								</div>
                                 
                                 <!-- ================= Description [start] =================== -->
-								<div class="form-group"  style="font-size: 18px" >
-                                    <label for="title"><?= $lang['Description']?></label>
-                                    <textarea id="description" name="description" class="form-control" rows="4" style="font-size: 18px" size="20" maxlength="1000" placeholder="<?= $lang['Place_Des']?>" value="<?php if(isset($description)) echo $description ?>"></textarea>
+								<div class="label-fontsize form-group" >
+                                    <label for="title"><?= $lang['ADD_VIDEO_FORM_DES'] ?></label>
+                                    <textarea id="description" name="description" class="form-control" rows="4" style="font-size: 18px" size="20" maxlength="1000" placeholder="<?= $lang['ADD_VIDEO_FORM_DES_TEXT']?>" value="<?php if(isset($description)) echo $description ?>"></textarea>
 								<?php if (isset($errors) && in_array('description', $errors)) : ?>
-                                    <div class='alert alert-warning' style='font-size: 16px; padding: 5px 5px 5px 12px; margin-top: 15px'>
-                                        <p><?= $lang['AD_Description_required']?></p>
+                                    <div class='message alert alert-warning'>
+                                        <p><?= $lang['ADD_VIDEO_FORM_DES_REQUIRED']?></p>
                                     </div>
                                 <?php endif; ?>
 								</div>
 								
                                 <!-- ================= Status: default is 0 [start] ===================== -->
-                                <div class="form-group" style="font-size: 18px">
-				                    <label><?= $lang['Select_Status']?></label>
+                                <div class="label-fontsize form-group" >
+				                    <label><?= $lang['ADD_VIDEO_FORM_STATUS']?></label>
 
-				                    <select name="status" class="form-control" style="font-size: 18px; height: 44px">
-				                        <option value="0"><?= $lang['Inactive']?></option>
-				                        <option value="1"><?= $lang['Active']?></option>
+				                    <select name="status" class="label-fontsize form-control" style="height: 44px">
+				                        <option value="0"><?= $lang['ADD_VIDEO_FORM_STATUS_VAL_0']?></option>
+				                        <option value="1"><?= $lang['ADD_VIDEO_FORM_STATUS_VAL_1']?></option>
 				                    </select>
 				                </div>
                                 
 								<!-- ================= Submit & Reset Button [start] ===================== -->
 								<center >
-									<input type="submit" name="submit" class="btn btn-success" style="font-size: 18px; height: 44px; margin-right: 10px"  value="<?= $lang['Submit']?>">
-									<button type="reset" class="btn btn-danger" style="font-size: 18px; height: 44px"><?= $lang['Reset']?></button>
+									<input type="submit" name="submit" class="btncustom btn btn-success" style="margin-right: 5px"  value="<?= $lang['BUTTON_ADD'] ?>">
+                                    <input type="reset" class="btncustom btn btn-warning" style="margin-right: 5px" value="<?= $lang['BUTTON_RESET'] ?>">
+                                    <input type="button" class="btncustom btn btn-danger" onclick="window.history.back();" value="<?= $lang['BUTTON_BACK'] ?>">
 								</center>
                                 
 							</form> <!-- END FORM ADD VIDEOS-->				 
